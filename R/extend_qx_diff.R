b912b60422e5d69e4847df49a6d8ae1b8fa7db20
Spencer Pease <spease@uw.edu>
2022-08-19 10:26:05 -0700

    feat: Update `extend_qx_diff()`
    * Use `age_end`
    * Remove explicit data.table use
    * rename `start_age` -> `base_age`

--- R/extend_qx_diff.R
+++ R/extend_qx_diff.R
@@ -10,7 +10,7 @@
 #' @export
 extend_qx_diff <- function(empir_lt, hmd_qx_results, by_vars) {
 
-  empir_lt <- data.table::copy(empir_lt)
+  empir_lt <- copy(empir_lt)
 
   ## remove any old age groups with qx > 1
   empir_lt[
@@ -27,7 +27,7 @@ extend_qx_diff <- function(empir_lt, hmd_qx_results, by_vars) {
 
   ## Drop old age groups after qx starts to decrease, if this happens
   ## Make sure we keep 65 -- this is the lowest age we extend from
-  data.table::setorderv(empir_lt, cols = c(by_vars, "age_start"))
+  setorderv(empir_lt, cols = c(by_vars, "age_start", "age_end"))
 
   empir_lt[
     age_start >= 60,
@@ -55,13 +55,13 @@ extend_qx_diff <- function(empir_lt, hmd_qx_results, by_vars) {
   empir_lt[, qx_change := NULL]
 
   ## Start should now be the last age that you have data for
-  empir_lt[, start_age := max(age_start), by = by_vars]
+  empir_lt[, base_age := max(age_start), by = by_vars]
 
   ## Drop all data where the max age in the series is less than 65 years -- need those years to do the rest of the extension
-  empir_lt <- empir_lt[start_age >= 65]
+  empir_lt <- empir_lt[base_age >= 65]
 
   ## Expand grid to get all the old ages
-  baseline_grid <- unique(empir_lt[, c(..by_vars, "start_age")], by = by_vars)
+  baseline_grid <- unique(empir_lt[, c(..by_vars, "base_age")], by = by_vars)
   baseline <- list()
 
   new_ages <- c(0, 1, seq(5, 105, 5))
@@ -71,77 +71,77 @@ extend_qx_diff <- function(empir_lt, hmd_qx_results, by_vars) {
     baseline[[list_item]][, age_start := new_age]
   }
 
-  baseline_grid <- data.table::rbindlist(baseline)
+  baseline_grid <- rbindlist(baseline)
 
   ## Merge everything together to get consistent age-series throughout
   empir_lt <- merge(
     baseline_grid,
     empir_lt,
-    by = c(by_vars, "age_start", "start_age"),
+    by = c(by_vars, "age_start", "base_age"),
     all.x = T
   )
 
   ## Generate logit qx and logit qx diff
   empir_lt[, logit_qx := demUtils::logit(qx)]
-  data.table::setorderv(empir_lt, c(by_vars, "age_start"))
+  setorderv(empir_lt, c(by_vars, "age_start", "age_end"))
   empir_lt[
     ,
-    diff_logit_qx_to := logit_qx - data.table::shift(logit_qx, type = "lag"),
+    diff_logit_qx_to := logit_qx - shift(logit_qx, type = "lag"),
     by = by_vars
   ]
 
   ## Format HMD regression parameters long instead of wide
-  hmd_qx_results <- data.table::copy(hmd_qx_results)
-  empir_lt <- merge(empir_lt, hmd_qx_results, by = c("sex", "age_start"), all.x = T)
+  empir_lt[hmd_qx_results, age_end := i.age_end, on = "age_start"]
+  empir_lt <- merge(empir_lt, hmd_qx_results, by = c("sex", "age_start", "age_end"), all.x = T)
 
   ## Iterate over age to recursively predict logit qx for next age group
   ## diff_logit_qx_to is the qx-difference in logit space TO age aa from the previous age
   ## diff_logit_qx_from is the qx-difference in logit space FROM age aa to the next age
-  data.table::setorderv(empir_lt, cols = c(by_vars, "age_start"))
+  setorderv(empir_lt, cols = c(by_vars, "age_start", "age_end"))
 
   for (aa in seq(65, 100)) {
 
     empir_lt[
-      start_age == age_start & age_start == aa,
+      base_age == age_start & age_start == aa,
       pred_logit_qx := logit_qx
     ]
     empir_lt[
-      start_age <= aa & age_start == aa,
+      base_age <= aa & age_start == aa,
       diff_logit_qx_from := intercept + slope * diff_logit_qx_to
     ]
     empir_lt[
-      start_age <= aa,
-      diff_logit_qx_to := data.table::shift(diff_logit_qx_from, type = "lag"),
+      base_age <= aa,
+      diff_logit_qx_to := shift(diff_logit_qx_from, type = "lag"),
       by = by_vars
     ]
     empir_lt[
-      start_age <= aa,
-      pred_logit_qx := data.table::fifelse(
+      base_age <= aa,
+      pred_logit_qx := fifelse(
         !is.na(pred_logit_qx),
         pred_logit_qx,
-        data.table::shift(pred_logit_qx, type = "lag") + diff_logit_qx_to
+        shift(pred_logit_qx, type = "lag") + diff_logit_qx_to
       ),
       by = by_vars
     ]
 
   }
 
-  empir_lt[age_start > start_age, logit_qx := pred_logit_qx]
-  empir_lt[age_start > start_age, qx := demUtils::invlogit(logit_qx)]
+  empir_lt[age_start > base_age, logit_qx := pred_logit_qx]
+  empir_lt[age_start > base_age, qx := demUtils::invlogit(logit_qx)]
 
-  data.table::setorderv(empir_lt, c(by_vars, "age_start"))
+  setorderv(empir_lt, c(by_vars, "age_start"))
 
   ## Set qx to 0.99 if >= 1
   empir_lt[qx >= 1, qx := 0.99]
 
   ## Add age end back
-  empir_lt[is.na(age_end) & age_start >= start_age, age_end := age_start + 5]
+  empir_lt[is.na(age_end) & age_start >= base_age, age_end := age_start + 5]
 
   ## subset variables
-  empir_lt[, c("diff_logit_qx_to", "diff_logit_qx_from", "slope", "intercept", "pred_logit_qx", "start_age", "logit_qx") := NULL]
+  empir_lt[, c("diff_logit_qx_to", "diff_logit_qx_from", "slope", "intercept", "pred_logit_qx", "base_age", "logit_qx") := NULL]
 
   ## Regenerate lx
-  data.table::setkeyv(empir_lt, c(by_vars, "age_start"))
+  setkeyv(empir_lt, c(by_vars, "age_start"))
   demCore::gen_lx_from_qx(empir_lt, c(by_vars, "age_start", "age_end"))
 
   ## Format and output
